45f2d4e18ffefd7a278e554a4016ea7d0ec89280
JustSalomon <gababeast1337@gmail.com>
2024-09-11 21:46:33 +0500

    add data

---
+++ import_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO students (name, last_survey_date) VALUES
+('Alice Smith', NULL),
+('Bob Johnson', NULL),
+('Charlie Brown', NULL),
+('Michael Jackson', NULL),
+('Freddie Anderson', NULL);
+
+INSERT INTO teachers (name) VALUES
+('Emily Davis'),
+('John Lee'),
+('Ip Man'),
+('Bruce Lee');
